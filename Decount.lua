7ecd6a7a2453cd03756a2f92959ecc1ad5a8561a
tekkub <tekkub@gmail.com>
2009-08-02 17:52:47 -0600

    More pattern tweaks

--- Decount.lua
+++ Decount.lua
@@ -4,7 +4,9 @@ local filterstrings = {
 	"%d+%. %S+%s*%d+ %([%d.]+, [%d.]+%%%)", -- Recount
 --~ 	"^(%d+). (.*)$", -- Also Recount
 	"%d+%. - [%d.]+%%% %S+%s*%d+",
-	"%d+. %S+%s[%d.]+ %([%d.]+%)", -- 10. Khal 2397.9 (5.8%)
+	"%d+%.%s+%S+%s+[%d.]+ %([%d.]+%)", -- 10. Khal 2397.9 (5.8%)
+--	"%d+%.%s+%S+%s+<%S+>%s+[%d.]+%s+%([%d.]+%)", -- 2. Bonehead <Demante>  129.9 (3.9%)
+	"%d+%.%s+%S+%s+[%d.]+", -- 10. Khal 2397.9
 }
 
 
@@ -16,3 +18,4 @@ end
 for _,event in pairs{"CHAT_MSG_YELL", "CHAT_MSG_WHISPER", "CHAT_MSG_SAY", "CHAT_MSG_RAID", "CHAT_MSG_GUILD", "CHAT_MSG_OFFICER", "CHAT_MSG_PARTY"} do
 	ChatFrame_AddMessageEventFilter(event, filter)
 end
+
